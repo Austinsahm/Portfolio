d4d341e0aa9c8cc4f0bac18a2c17ce0d853e05b1
austinsamuel <austinsamuel733@gmail.com>
2022-10-31 23:11:35 +0100

    History

--- src/app/more/know-more.component.ts
+++ src/app/more/know-more.component.ts
@@ -4,12 +4,14 @@ import { HeaderComponent } from '../widgets/Know-more_widgets/header/header.comp
 import { NavbarComponent } from '../widgets/Know-more_widgets/navbar/navbar.component';
 import { InfoComponent } from '../widgets/Know-more_widgets/info/info.component';
 import { MyResumeComponent } from '../widgets/Know-more_widgets/my-resume/my-resume.component';
+import { HistoryComponent } from '../widgets/Know-more_widgets/history/history.component';
 
 @Component({
   selector: 'app-know-more',
   standalone: true,
   imports: [CommonModule, HeaderComponent,
-    NavbarComponent, InfoComponent, MyResumeComponent],
+    NavbarComponent, InfoComponent, 
+    MyResumeComponent, HistoryComponent],
   templateUrl: './know-more.component.html',
   styleUrls: ['./know-more.component.css'],
 })
